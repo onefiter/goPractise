b728bfa3692ae3f7b4593760f0c1b955e30e463a
onefiter <onefiter@gmail.com>
2022-12-13 18:49:00 +0800

    上传图片

---
+++ blog-service/internal/service/upload.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"errors"
+	"mime/multipart"
+	"os"
+
+	"github.com/blog-service/global"
+	"github.com/blog-service/pkg/upload"
+)
+
+type FileInfo struct {
+	Name      string
+	AccessUrl string
+}
+
+func (scv *Service) UploadFile(fileType upload.FileType, file multipart.File, fileHeader *multipart.FileHeader) (*FileInfo, error) {
+	fileName := upload.GetFileName(fileHeader.Filename)
+
+	if !upload.CheckContainExt(fileType, fileName) {
+		return nil, errors.New("file suffix is not supported")
+	}
+
+	if upload.CheckMaxSize(fileType, file) {
+		return nil, errors.New("exceeded maximum file limit")
+	}
+
+	uploadSavePath := upload.GetSavePath()
+	if upload.CheckSavePath(uploadSavePath) {
+		if err := upload.CreateSavePath(uploadSavePath, os.ModePerm); err != nil {
+			return nil, errors.New("failed to create save directory")
+		}
+	}
+	if upload.CheckPermission(uploadSavePath) {
+		return nil, errors.New("insufficient file permissions")
+	}
+
+	dst := uploadSavePath + "/" + fileName
+	if err := upload.SaveFile(fileHeader, dst); err != nil {
+		return nil, err
+	}
+
+	accessUrl := global.AppSetting.UploadServerUrl + "/" + fileName
+	return &FileInfo{Name: fileName, AccessUrl: accessUrl}, nil
+}
---
+++ blog-service/pkg/upload/file.go
@@ -0,0 +1,99 @@
+package upload
+
+import (
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/blog-service/global"
+	"github.com/blog-service/pkg/util"
+)
+
+type FileType int
+
+const TypeImage FileType = iota + 1
+
+func GetFileName(name string) string {
+	ext := GetFileExt(name)
+	fileName := strings.TrimSuffix(name, ext)
+	fileName = util.EncodeMd5(fileName)
+	return fileName + ext
+}
+
+func GetFileExt(name string) string {
+	return path.Ext(name)
+}
+
+func GetSavePath() string {
+	return global.AppSetting.UploadSavePath
+}
+
+func CheckSavePath(dst string) bool {
+	_, err := os.Stat(dst)
+	return os.IsNotExist(err)
+}
+
+func CheckContainExt(t FileType, name string) bool {
+	ext := GetFileExt(name)
+	ext = strings.ToUpper(ext)
+	switch t {
+	case TypeImage:
+		for _, allowExt := range global.AppSetting.UploadImageAllowExts {
+			if strings.ToUpper(allowExt) == ext {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func CheckMaxSize(t FileType, f multipart.File) bool {
+	content, _ := ioutil.ReadAll(f)
+	size := len(content)
+	switch t {
+	case TypeImage:
+		if size >= global.AppSetting.UploadImageMaxSize*1024*1024 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func CheckPermission(dst string) bool {
+	_, err := os.Stat(dst)
+	return os.IsPermission(err)
+}
+
+func CreateSavePath(dst string, perm os.FileMode) error {
+	err := os.MkdirAll(dst, perm)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SaveFile(file *multipart.FileHeader, dst string) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	_, err = io.Copy(out, src)
+
+	return err
+}
