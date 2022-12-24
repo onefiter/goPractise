c1d249e0da61d96a21b73402d6d9e42157ac7c3c
onefiter <onefiter@gmail.com>
2022-12-24 23:27:54 +0800

    增加jwt中间件

--- blog-service/internal/routers/router.go
+++ blog-service/internal/routers/router.go
@@ -30,6 +30,7 @@ func NewRouter() *gin.Engine {
 	r.POST("/auth", api.GetAuth)
 
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(middleware.JWT())
 	{
 		apiv1.POST("/tags", tag.Create)
 		apiv1.DELETE("/tags/:id", tag.Delete)
