36f608f2bd98a39ebfe15e3ba4247293196c66da
onefiter <onefiter@gmail.com>
2023-01-04 22:23:50 +0800

    udpate

--- hade/main.go
+++ hade/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/goPractise/hade/framework"
 	"github.com/goPractise/hade/framework/middleware"
@@ -16,5 +21,22 @@ func main() {
 		Handler: core,
 		Addr:    "localhost:8080",
 	}
-	server.ListenAndServe()
+
+	go func() {
+
+		server.ListenAndServe()
+	}()
+
+	// 等待的 Goroutine 等待信号量、
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT， SIGTERM，SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	// 这里会阻塞当前 Goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
 }
