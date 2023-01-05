db49a1c8ce79bbdd892fc6e4b0c1ff5d53f976e5
onefiter <onefiter@gmail.com>
2023-01-05 22:12:50 +0800

    net/http

---
+++ pkg/net/net.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
