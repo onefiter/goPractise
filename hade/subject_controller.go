2be56735a7768466d8f97edabd607850a0221dbb
onefiter <onefiter@gmail.com>
2023-01-14 22:06:21 +0800

    fix

--- hade/subject_controller.go
+++ hade/subject_controller.go
@@ -3,31 +3,31 @@ package main
 import "github.com/goPractise/hade/framework"
 
 func SubjectAddController(c *framework.Context) error {
-	c.Json(200, "ok, SubjectAddController")
+	c.Json("ok, SubjectAddController")
 	return nil
 }
 
 func SubjectListController(c *framework.Context) error {
-	c.Json(200, "ok, SubjectListController")
+	c.Json("ok, SubjectListController")
 	return nil
 }
 
 func SubjectDelController(c *framework.Context) error {
-	c.Json(200, "ok, SubjectDelController")
+	c.Json("ok, SubjectDelController")
 	return nil
 }
 
 func SubjectUpdateController(c *framework.Context) error {
-	c.Json(200, "ok, SubjectUpdateController")
+	c.Json("ok, SubjectUpdateController")
 	return nil
 }
 
 func SubjectGetController(c *framework.Context) error {
-	c.Json(200, "ok, SubjectGetController")
+	c.Json("ok, SubjectGetController")
 	return nil
 }
 
 func SubjectNameController(c *framework.Context) error {
-	c.Json(200, "ok, SubjectNameController")
+	c.Json("ok, SubjectNameController")
 	return nil
 }
--- hade/user_controller.go
+++ hade/user_controller.go
@@ -3,6 +3,6 @@ package main
 import "github.com/goPractise/hade/framework"
 
 func UserLoginController(c *framework.Context) error {
-	c.Json(200, "ok, UserLoginController")
+	c.Json("ok, UserLoginController")
 	return nil
 }
