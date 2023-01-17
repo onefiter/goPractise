0624099e4401606b986bb82b4ba6faf4d5c1eea5
onefiter <onefiter@gmail.com>
2023-01-18 00:02:05 +0800

    dirs

---
+++ hade/framework/cobra/hade_command.go
@@ -0,0 +1,13 @@
+package cobra
+
+import "github.com/goPractise/hade/framework"
+
+// SetContainer 设置服务容器
+func (c *Command) SetContainer(container framework.Container) {
+	c.container = container
+}
+
+// GetContainer 获取容器
+func (c *Command) GetContainer() framework.Container {
+	return c.Root().container
+}
---
+++ hade/framework/util/exec.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"os"
+	"syscall"
+)
+
+// GetExecDirectory 获取当前执行程序目录
+func GetExecDirectory() string {
+	file, err := os.Getwd()
+	if err == nil {
+		return file + "/"
+	}
+	return ""
+}
+
+// CheckProcessExist Will return true if the process with PID exists.
+func CheckProcessExist(pid int) bool {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+
+	err = process.Signal(syscall.Signal(0))
+
+	return err == nil
+}
