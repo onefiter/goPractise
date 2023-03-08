0d07f5445c13a93b3eae82373b8262508efe2123
onefiter <onefiter@gmail.com>
2023-03-08 18:34:39 +0800

    类型断言

--- pkg/interface/main.go
+++ pkg/interface/main.go
@@ -8,6 +8,12 @@ type Car interface {
 	Drive()
 }
 
+
+type TrafficTool interface {
+	Drive()
+	Run()
+}
+
 type Truck  struct {
 	Model string
 }
@@ -19,6 +25,27 @@ func (t Truck) Drive() {
 func main() {
 	// c并不是简单地转为了Truck类型
 	var c Car = Truck{}
-	fmt.Println(c)
+
+	// // 断言作用1：类型转换
+	// t := c.(Truck)
+	// 此时t是Truck类型
+	// fmt.Println(t)
 	
+
+	// 比较了底层的实现，和目标的接口是不是实现了目标接口的方法
+	// 当TrafficTool中的接口c中并没有完全实现，就意味着不能转
+	// tt := c.(TrafficTool)
+	// fmt.Println(tt)
+
+	switch c.(type) {
+	case TrafficTool:
+		fmt.Println("TrafficTool")
+	case Truck:
+		fmt.Println("Truck")
+	case Car:
+		fmt.Println("Car")
+	default:
+		fmt.Println("None")
+		
+	}
 }
