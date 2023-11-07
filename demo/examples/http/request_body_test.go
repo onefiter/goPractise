24be26e6cb1bd4f07bd2f3f3d52e34000960893c
onefiter <onefiter@gmail.com>
2023-11-07 22:29:36 +0800

    request_body_test

---
+++ demo/examples/http/request_body_test.go
@@ -0,0 +1,77 @@
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hi, this is home page")
+}
+
+func readBodyOnce(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "read body failed: %v", err)
+		// 记住要返回，不然就还会执行后面的代码
+		return
+	}
+	// 类型转换，将 []byte 转换为 string
+	fmt.Fprintf(w, "read the data: %s \n", string(body))
+
+	// 尝试再次读取，啥也读不到，但是也不会报错
+	body, err = io.ReadAll(r.Body)
+	if err != nil {
+		// 不会进来这里
+		fmt.Fprintf(w, "read the data one more time got error: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "read the data one more time: [%s] and read data length %d \n", string(body), len(body))
+}
+
+func getBodyIsNil(w http.ResponseWriter, r *http.Request) {
+	if r.GetBody == nil {
+		fmt.Fprint(w, "GetBody is nil \n")
+	} else {
+		fmt.Fprintf(w, "GetBody not nil \n")
+	}
+}
+
+func queryParams(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	fmt.Fprintf(w, "query is %v\n", values)
+}
+
+func wholeUrl(w http.ResponseWriter, r *http.Request) {
+	data, _ := json.Marshal(r.URL)
+	fmt.Fprintf(w, string(data))
+}
+
+func header(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "header is %v\n", r.Header)
+}
+
+func form(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "before parse form %v\n", r.Form)
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "parse form error %v\n", r.Form)
+	}
+	fmt.Fprintf(w, "before parse form %v\n", r.Form)
+}
+
+func TestRequestBody(t *testing.T) {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/body/once", readBodyOnce)
+	http.HandleFunc("/body/multi", getBodyIsNil)
+	http.HandleFunc("/url/query", queryParams)
+	http.HandleFunc("/header", header)
+	http.HandleFunc("/wholeUrl", wholeUrl)
+	http.HandleFunc("/form", form)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
